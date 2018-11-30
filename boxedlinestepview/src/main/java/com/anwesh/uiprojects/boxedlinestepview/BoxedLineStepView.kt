b80364d3916ae6345954285a4138676bd1a1a7ac
Anwesh43 <anweshthecool0@gmail.com>
2018-11-30 09:54:59 +0530

    adding Float and Int extension methods

--- boxedlinestepview/src/main/java/com/anwesh/uiprojects/boxedlinestepview/BoxedLineStepView.kt
+++ boxedlinestepview/src/main/java/com/anwesh/uiprojects/boxedlinestepview/BoxedLineStepView.kt
@@ -18,6 +18,18 @@ val lines : Int = 5
 val sizeFactor : Int = 3
 val strokeFactor : Int = 120
 val color : Int = Color.parseColor("#283593")
+val scDiv : Double = 0.51
+val scGap : Float = 0.05f
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - n.getInverse() * i)) * n
+
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
+
+fun Float.updateScale(dir : Int, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
 
 class BoxedLineStepView(ctx : Context) : View(ctx) {
 
