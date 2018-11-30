fd00fb79b1af71ff1fc3769620038fa3e66354d2
Anwesh43 <anweshthecool0@gmail.com>
2018-11-30 09:47:36 +0530

    adding libraries and defining constants

--- boxedlinestepview/src/main/java/com/anwesh/uiprojects/boxedlinestepview/BoxedLineStepView.kt
+++ boxedlinestepview/src/main/java/com/anwesh/uiprojects/boxedlinestepview/BoxedLineStepView.kt
@@ -3,3 +3,18 @@ package com.anwesh.uiprojects.boxedlinestepview
 /**
  * Created by anweshmishra on 30/11/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 5
+val sizeFactor : Int = 3
+val strokeFactor : Int = 120
+val color : Int = Color.parseColor("#283593")
