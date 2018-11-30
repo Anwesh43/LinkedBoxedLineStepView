0ff05f1be5065eea1f3a8b6716b4b0a163e80a30
Anwesh43 <anweshthecool0@gmail.com>
2018-11-30 11:54:31 +0530

    adding View in Activity

--- app/src/main/java/com/anwesh/uiprojects/linkedboxedlinestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedboxedlinestepview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.linkedboxedlinestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.boxedlinestepview.BoxedLineStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        BoxedLineStepView.create(this)
     }
 }
