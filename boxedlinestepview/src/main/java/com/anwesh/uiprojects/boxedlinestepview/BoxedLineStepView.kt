f577c84e975e065e0a2fbcd8d04c3c2bf096dd4d
Anwesh43 <anweshthecool0@gmail.com>
2018-11-30 11:41:59 +0530

    adding BLSNode class

--- boxedlinestepview/src/main/java/com/anwesh/uiprojects/boxedlinestepview/BoxedLineStepView.kt
+++ boxedlinestepview/src/main/java/com/anwesh/uiprojects/boxedlinestepview/BoxedLineStepView.kt
@@ -120,4 +120,49 @@ class BoxedLineStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLSNode(var i : Int, val state : State = State()) {
+
+        private var next : BLSNode? = null
+
+        private var prev : BLSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BLSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLSNode {
+            var curr : BLSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
