4097c7369c1ddaed388fdaa8077e589ddd32d110
Anwesh43 <anweshthecool0@gmail.com>
2018-11-30 09:49:40 +0530

    adding BoxedLiineStepView class

--- boxedlinestepview/src/main/java/com/anwesh/uiprojects/boxedlinestepview/BoxedLineStepView.kt
+++ boxedlinestepview/src/main/java/com/anwesh/uiprojects/boxedlinestepview/BoxedLineStepView.kt
@@ -18,3 +18,21 @@ val lines : Int = 5
 val sizeFactor : Int = 3
 val strokeFactor : Int = 120
 val color : Int = Color.parseColor("#283593")
+
+class BoxedLineStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
